8b59d353397ea349811a9f6ea67e682dfa5c7e02
Milagro Scardigno <mdscardigno@gmail.com>
2022-08-27 15:36:08 -0400

    practice with list

--- Program.cs
+++ Program.cs
@@ -15,7 +15,7 @@ namespace Collections
             var firstName = names[0];
             var secondName = names[1];
             var nameCount = names.Length;
-            Console.WriteLine("Total names: " + nameCount + ". " + "First Name on Index 0 and Second Name on Index 1: " + firstName + " " + secondName);
+            Console.WriteLine("Total names: " + nameCount + ". " + $"First Name on Index 0 is: {firstName} and Second Name on Index 1 is: {secondName}");
             // Console.WriteLine(names[42]); //outside bounds
             // the word new indicates we are creating a new object of a specific class.
             //The parenthesis after <string>() is where we are to put info on how to create the list and is a required syntax
@@ -23,17 +23,25 @@ namespace Collections
             //The List is still accessed with the []
             var firstNameOnList = namesList[0];
             Console.WriteLine(firstNameOnList);
-            System.Console.WriteLine(namesList.Count);
+            System.Console.WriteLine($"How many names in the list before adding new member: {namesList.Count}.");
             namesList.Add("Mila");
-            System.Console.WriteLine(namesList.Count);
+            System.Console.WriteLine($"How many names in the list: {namesList.Count}.");
 
             var listOfScores = new List<int>();
             //Lists must contain items of the same type
             listOfScores.Add(12);
-            listOfScores.Add(12);
-            listOfScores.Add(12);
+            listOfScores.Add(100);
+            listOfScores.Add(55);
+            listOfScores.Add(44);
 
+        var lastIndex = listOfScores.Count - 1;
+        var lastScore = listOfScores[lastIndex];
 
+        var indexOfFiftyFive = listOfScores.IndexOf(55);
+        System.Console.WriteLine($"Found 55 at index {indexOfFiftyFive}");
+
+        var indexOfFortyTwo = listOfScores.IndexOf(42);
+        System.Console.WriteLine($"Found 42 at the index {indexOfFortyTwo}"); //did not find anything. This will print Found 42 at the index -1
         }
     }
 }
